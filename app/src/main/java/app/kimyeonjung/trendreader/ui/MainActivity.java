891ed4a52a30f3ec41ea3b688d4554589d80808d
YeonJung Kim <duswnd25@gmail.com>
2017-12-06 00:12:00 +0900

    icon 적용
    styleable의 format이 문제였다
    이를 원본으로 교체하니 정상 작동함

--- app/src/main/java/app/kimyeonjung/trendreader/ui/MainActivity.java
+++ app/src/main/java/app/kimyeonjung/trendreader/ui/MainActivity.java
@@ -1,10 +1,13 @@
 package app.kimyeonjung.trendreader.ui;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,6 +17,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.ogaclejapan.smarttablayout.SmartTabLayout;
 import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
@@ -32,13 +37,8 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -62,6 +62,23 @@ public class MainActivity extends AppCompatActivity
         viewPager.setAdapter(tabAdapter);
 
         SmartTabLayout viewPagerTab = findViewById(R.id.viewpager_tab);
+
+        final LayoutInflater inflater = LayoutInflater.from(viewPagerTab.getContext());
+        final Resources res = viewPagerTab.getContext().getResources();
+
+        viewPagerTab.setCustomTabView((container, position, adapter) -> {
+            ImageView icon = (ImageView) inflater.inflate(R.layout.smart_tab_icon, container,
+                    false);
+            switch (position) {
+                case 0:
+                    icon.setImageDrawable(res.getDrawable(R.drawable.ic_feed));
+                    break;
+                default:
+                    throw new IllegalStateException("Invalid position: " + position);
+            }
+            return icon;
+        });
+
         viewPagerTab.setViewPager(viewPager);
     }
 
