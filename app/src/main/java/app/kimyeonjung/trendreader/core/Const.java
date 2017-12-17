8e0319da254b331b10dfca570f930dee4ea7cc21
YeonJung Kim <duswnd25@gmail.com>
2017-12-18 01:42:30 +0900

    새 화면

--- app/src/main/java/app/kimyeonjung/trendreader/core/Const.java
+++ app/src/main/java/app/kimyeonjung/trendreader/core/Const.java
@@ -8,4 +8,12 @@ public class Const {
     public class DB {
         public static final String BOOK_MAKR = "bookmark.realm";
     }
+
+    public class INTENT {
+        public static final String BLOG_TITLE = "blog_title";
+        public static final String POST_URL = "post_url";
+        public static final String POST_CONTENT = "post_content";
+        public static final String FAVICON_URL = "favicon_url";
+        public static final String POST_TITLE = "post_title";
+    }
 }
--- app/src/main/java/app/kimyeonjung/trendreader/data/feed/fetch/FeedAdapter.java
+++ app/src/main/java/app/kimyeonjung/trendreader/data/feed/fetch/FeedAdapter.java
@@ -2,8 +2,10 @@ package app.kimyeonjung.trendreader.data.feed.fetch;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.support.v7.graphics.Palette;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,7 +21,9 @@ import com.bumptech.glide.request.target.BitmapImageViewTarget;
 import java.util.LinkedList;
 
 import app.kimyeonjung.trendreader.R;
+import app.kimyeonjung.trendreader.core.Const;
 import app.kimyeonjung.trendreader.data.feed.FeedItem;
+import app.kimyeonjung.trendreader.ui.feed.DetailView;
 import me.grantland.widget.AutofitTextView;
 
 public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.ViewHolder> {
@@ -45,6 +49,17 @@ public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.ViewHolder> {
 
         FeedItem temp = feedList.get(position);
         holder.title.setText(temp.getPostTitle());
+
+        holder.container.setOnClickListener(view -> {
+            Intent intent = new Intent();
+            intent.putExtra(Const.INTENT.BLOG_TITLE, temp.getBlogName());
+            intent.putExtra(Const.INTENT.FAVICON_URL, temp.getFaviconUrl());
+            intent.putExtra(Const.INTENT.POST_TITLE, temp.getPostTitle());
+            intent.putExtra(Const.INTENT.POST_CONTENT, temp.getPostContent());
+            intent.putExtra(Const.INTENT.POST_URL, temp.getPostUrl());
+            context.startActivity(new Intent(context, DetailView.class));
+        });
+
 
         String content = temp.getPostContent();
         if (content.length() != 0) {
@@ -85,9 +100,11 @@ public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.ViewHolder> {
         private TextView description;
         private ImageView profile;
         private LinearLayout descriptionContainer;
+        private CardView container;
 
         ViewHolder(View view) {
             super(view);
+            container = view.findViewById(R.id.feed_container);
             profile = view.findViewById(R.id.feed_profile_image);
             title = view.findViewById(R.id.feed_title);
             description = view.findViewById(R.id.feed_description);
