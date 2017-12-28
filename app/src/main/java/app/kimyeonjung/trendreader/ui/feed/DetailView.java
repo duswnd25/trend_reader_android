776282bdd28ad4986df3c18529a631282b1cf521
YeonJung Kim <duswnd25@gmail.com>
2017-12-28 14:02:22 +0900

    커스텀 토스트

--- app/src/main/java/app/kimyeonjung/trendreader/ui/feed/DetailView.java
+++ app/src/main/java/app/kimyeonjung/trendreader/ui/feed/DetailView.java
@@ -4,56 +4,56 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.animation.GlideAnimation;
 import com.bumptech.glide.request.target.BitmapImageViewTarget;
+import com.muddzdev.styleabletoastlibrary.StyleableToast;
+
+import java.util.Date;
 
 import app.kimyeonjung.trendreader.R;
 import app.kimyeonjung.trendreader.core.Const;
+import app.kimyeonjung.trendreader.data.FeedItem;
+import io.realm.Realm;
 
 public class DetailView extends AppCompatActivity {
-    private Intent intent;
-    private String postUrl;
+    private FeedItem feedItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_view);
-        intent = getIntent();
 
-        Toolbar toolbar = findViewById(R.id.detail_toolbar);
-        toolbar.setTitle(intent.getStringExtra(Const.INTENT.POST_TITLE));
-        setSupportActionBar(toolbar);
+        feedItem = (FeedItem) getIntent().getSerializableExtra(Const.INTENT.POST_DATA);
 
         initView();
     }
 
     private void initView() {
 
+        Toolbar toolbar = findViewById(R.id.detail_toolbar);
+        toolbar.setTitle(feedItem.getPostTitle());
+        setSupportActionBar(toolbar);
+
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         boolean isPaletteUse = prefs.getBoolean(getString(R.string.pref_feed_palette_use), true);
 
-        String faviconUrl = intent.getStringExtra(Const.INTENT.FAVICON_URL);
-        String postContent = intent.getStringExtra(Const.INTENT.POST_CONTENT);
-        postUrl = intent.getStringExtra(Const.INTENT.POST_URL);
+        ((TextView) findViewById(R.id.detail_text)).setText(feedItem.getPostContent());
 
-        ((TextView) findViewById(R.id.detail_text)).setText(postContent);
-
-        Glide.with(this).load(faviconUrl).asBitmap()
+        Glide.with(this).load(feedItem.getFaviconUrl()).asBitmap()
                 .centerCrop().placeholder(R.mipmap.ic_launcher_round)
                 .into(new BitmapImageViewTarget(findViewById(R.id.detail_favicon)) {
                     @Override
@@ -73,6 +73,25 @@ public class DetailView extends AppCompatActivity {
 
     }
 
+    private void addBookMark() {
+
+        try (Realm realm = Realm.getInstance(Const.DB.getBookMarkConfig())) {
+
+            realm.beginTransaction();
+
+            FeedItem feed = realm.createObject(FeedItem.class);
+            feed = realm.copyToRealm(feedItem);
+            realm.commitTransaction();
+
+            new StyleableToast
+                    .Builder(this)
+                    .text(getString(R.string.action_bookmark_save))
+                    .textColor(Color.WHITE)
+                    .backgroundColor(getResources().getColor(R.color.colorPrimary))
+                    .show();
+        }
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -80,9 +99,10 @@ public class DetailView extends AppCompatActivity {
                 finish();
                 break;
             case R.id.action_bookmark:
+                addBookMark();
                 break;
             case R.id.action_link:
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(postUrl)));
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(feedItem.getPostUrl())));
                 break;
         }
         return super.onOptionsItemSelected(item);
@@ -91,6 +111,11 @@ public class DetailView extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.detail_menu, menu);
+        for (int index = 0; index < menu.size(); index++) {
+            Drawable icon = menu.getItem(index).getIcon();
+            icon.mutate();
+            icon.setColorFilter(getResources().getColor(R.color.colorPrimary), PorterDuff.Mode.SRC_IN);
+        }
         return true;
     }
 }
