871f99336febbdbe48f5d57a40ae63eed8627042
YeonJung Kim <duswnd25@gmail.com>
2017-12-30 01:06:42 +0900

    값 범위 조정

--- app/src/main/java/app/kimyeonjung/trendreader/core/prefs/NumberPickerPreference.java
+++ app/src/main/java/app/kimyeonjung/trendreader/core/prefs/NumberPickerPreference.java
@@ -16,8 +16,8 @@ import android.widget.NumberPicker;
 public class NumberPickerPreference extends DialogPreference {
 
     // allowed range
-    private static final int MAX_VALUE = 100;
-    private static final int MIN_VALUE = 0;
+    private static final int MAX_VALUE = 10;
+    private static final int MIN_VALUE = 1;
     // enable or disable the 'circular behavior'
     private static final boolean WRAP_SELECTOR_WHEEL = true;
 
