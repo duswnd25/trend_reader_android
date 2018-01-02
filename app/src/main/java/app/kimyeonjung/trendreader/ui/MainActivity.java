42a4f61aa0dacf8d7767301c8a23d6438e475020
YeonJung Kim <duswnd25@gmail.com>
2018-01-03 02:18:31 +0900

    색상

--- app/src/main/java/app/kimyeonjung/trendreader/ui/MainActivity.java
+++ app/src/main/java/app/kimyeonjung/trendreader/ui/MainActivity.java
@@ -39,6 +39,7 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setItemIconTintList(null);
         initTabView();
     }
 
