f902be2e73424a5203e9948d988e9f9ddbd8183d
YeonJung Kim <duswnd25@gmail.com>
2018-01-21 14:09:59 +0900

    library

---
+++ app/src/main/java/app/kimyeonjung/trendreader/data/about/LibraryAdapter.java
@@ -0,0 +1,47 @@
+package app.kimyeonjung.trendreader.data.about;
+
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.LinkedList;
+
+
+public class LibraryAdapter extends RecyclerView.Adapter<LibraryAdapter.ViewHolder> {
+
+    private LinkedList<String> itemList = new LinkedList<>();
+
+    public LibraryAdapter(LinkedList<String> itemList) {
+        this.itemList = itemList;
+    }
+
+    @Override
+    public LibraryAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(LibraryAdapter.ViewHolder holder, @SuppressLint("RecyclerView") final int position) {
+        holder.textView.setText(itemList.get(position));
+        holder.textView.setTextColor(Color.WHITE);
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemList.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView textView;
+
+        ViewHolder(View view) {
+            super(view);
+            textView = view.findViewById(android.R.id.text1);
+        }
+    }
+}
--- app/src/main/java/app/kimyeonjung/trendreader/ui/AboutApp.java
+++ app/src/main/java/app/kimyeonjung/trendreader/ui/AboutApp.java
@@ -2,9 +2,14 @@ package app.kimyeonjung.trendreader.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
+import java.util.LinkedList;
+
 import app.kimyeonjung.trendreader.R;
+import app.kimyeonjung.trendreader.data.about.LibraryAdapter;
 
 public class AboutApp extends AppCompatActivity {
 
@@ -16,5 +21,37 @@ public class AboutApp extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.about_app_toolbar);
         toolbar.setTitle(getString(R.string.title_about_dev));
         toolbar.setNavigationOnClickListener(v -> finish());
+
+        RecyclerView libraryListView = findViewById(R.id.about_app_recycler);
+        libraryListView.setHasFixedSize(true);
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
+        libraryListView.setLayoutManager(mLayoutManager);
+
+        LinkedList<String> itemList = new LinkedList<>();
+        itemList.add("ocm.android.support:appcompat-v7");
+        itemList.add("com.android.support:design");
+        itemList.add("com.android.support.constraint:constraint-layout");
+        itemList.add("com.android.support:support-v4");
+        itemList.add("com.android.support:cardview-v7");
+        itemList.add("com.android.support:palette-v7");
+        itemList.add("com.android.support:recyclerview-v7");
+        itemList.add("com.android.support:multidex");
+        itemList.add("com.android.support:support-vector-drawable");
+        itemList.add("com.squareup:otto");
+        itemList.add("com.squareup.okhttp:okhttp");
+        itemList.add("com.github.bumptech.glide:glide");
+        itemList.add("com.github.jrvansuita:MaterialAbout");
+        itemList.add("com.github.sharish:ShimmerRecyclerView");
+        itemList.add("com.muddzdev:styleabletoast");
+        itemList.add("com.squareup:otto");
+        itemList.add("com.yalantis:phoenix");
+        itemList.add("io.realm:android-adapters");
+        itemList.add("com.crashlytics.sdk.android:crashlytics");
+        itemList.add("com.google.firebase:firebase-core:");
+        itemList.add("com.google.firebase:firebase-messaging");
+
+        LibraryAdapter galleryAdapter;
+        galleryAdapter = new LibraryAdapter(itemList);
+        libraryListView.setAdapter(galleryAdapter);
     }
 }
